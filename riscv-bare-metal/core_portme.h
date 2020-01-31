0b041e859eaf244ce11a8f6afbc59649aae79a5d
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-01-31 14:17:43 +0000

    [riscv-baremetal]: Remove the NULL as it's already defined elsewhere

--- riscv-bare-metal/core_portme.h
+++ riscv-bare-metal/core_portme.h
@@ -92,7 +92,6 @@ typedef unsigned char ee_u8;
 typedef unsigned int ee_u32;
 typedef unsigned long ee_ptr_int;
 typedef size_t ee_size_t;
-#define NULL ((void *)0)
 /* align_mem :
 	This macro is used to align an offset to point to a 32b value. It is used in the Matrix algorithm to initialize the input memory blocks.
 */
